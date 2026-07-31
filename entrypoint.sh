8ead19be7da25a055f0940b84b453fb6f2370e62
Ddfathu <108730817+Ddfathu@users.noreply.github.com>
2026-07-31 19:28:09 +0700

    Rename RLWY_DOM to SNI in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -141,7 +141,7 @@ cloudflared tunnel --url "http://127.0.0.1:$PUBLIC_PORT" --protocol http2 > /tmp
         fi
 
         CUSTOM_DOM="${D:-}"
-        RLWY_DOM="${RLWY_PROXY:-}"
+        SNI="${RLWY_PROXY:-}"
 
         cat <<EOF > /tmp/server_stats.json
 {
@@ -153,7 +153,7 @@ cloudflared tunnel --url "http://127.0.0.1:$PUBLIC_PORT" --protocol http2 > /tmp
   "ssh_online": "👥 $SSH_ONLINE Active",
   "user_list_details": "$USER_DETAILS_LIST",
   "custom_domain": "$CUSTOM_DOM",
-  "railway_proxy": "$RLWY_DOM"
+  "railway_proxy": "$SNI"
 }
 EOF
         sleep 2
